b9bc203208c7f64b613d7c5a0ac982cca0815365
Colin Fuller-Bennett <cfbennett3169@gmail.com>
2021-03-08 23:38:47 -0500

    updated the landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,51 @@
 <template>
 
-<div>
+<div>   
+    <div class="video-section">
+      <div class="video-container">
+         <video autoplay loop muted src="/PoolDay.mp4"></video>
+      </div>
+
+<div class="site-content pa-3 rounded">
+
+<v-row justify-md="start" justify="center" align="center">
+  <v-col cols="6" md="4">
+   <div class="d-flex justify-content-center"> <v-img width="350px" src="/icon.png"></v-img></div> 
+  </v-col>
+  <v-col cols="auto" >
+     <p class="ma-0 display-1 text--primary mt-n12 mt-md-0">Island Blue Pools</p>   
+  </v-col>
+</v-row>
+
    
+<p>We help you learn to swim quickly and with confidence</p>
 
+        
+<v-btn width="50%" x-large color="primary" class="text-capitalize" target="blank" href="https://beta.oneshapp.com/islandbluepools">Get Started</v-btn>
 
+<v-btn text width="50%" x-large color="primary" class="text-capitalize" target="blank" href="https://beta.oneshapp.com/islandbluepools">Shop Swim Gear</v-btn>
 
-    <v-row justify="space-between">
-      <v-col cols="3">
-        <v-img width="50px" src="/icon.png"></v-img>
-      </v-col>
-      <v-col cols="3">
-       <v-btn target="blank" href="https://beta.oneshapp.com/islandbluepools">Shop Swin Gear</v-btn> 
-      </v-col>
-    </v-row>
-
-    <div>
-
-Gonna a use a video background here! 
-
-        Island Blue Pools
-
-        We help you learn to swim quickly and with confidence
-
-Get Started (Takes them to link to sign up)
+</div>
 
     </div>
 
-    <div>
+    <v-container>
 
-      <v-row>
-        <v-col>
+      <v-row justify="center">
+        <v-col cols="12" md="4">
 
-                <v-card>
+                <v-card height="500">
+                   <v-img
+      height="250"
+      src="/quick.jpg"
+    ></v-img>
 
-        Experience Swift Results
-Our knowledgeable and
+                  <v-card-text>
+      <p class="display-1 text--primary">
+      Experience Swift Results
+      </p>
+      <div class="text--primary">
+       Our knowledgeable and
 professional team of trained
 swim coaches will guide you
 through the basics of
@@ -43,34 +54,62 @@ increase your confidence in
 the water and ensure you
 experience results in the
 first few lessons
+      </div>
+    </v-card-text>
+
+       
 
       </v-card>
 
         </v-col>
 
-        <v-col>
+        <v-col cols="12" md="4">
 
-                <v-card>
-        Have Fun While Learning
-We want you to enjoy
+                <v-card height="500">
+
+                   <v-img
+      height="250"
+      src="/fun.png"
+    ></v-img>
+
+                  <v-card-text>
+      <p class="display-1 text--primary">
+      Have Fun While Learning
+      </p>
+      <div class="text--primary">
+     We want you to enjoy
 yourselves while learning to
 swim, so we incorporate fun
 activities into our lessons.
 Water exercises is also a
 great way to work on your
 fitness.
+      </div>
+    </v-card-text>
+
+
+        
+
 
       </v-card>
 
         </v-col>
 
-        <v-col>
+        <v-col cols="12" md="4">
 
-                <v-card>
+                <v-card height="500">
 
+                                     <v-img
+      height="250"
+      src="/pool.webp"
+    ></v-img>
+
+                  <v-card-text>
+      <p class="display-1 text--primary">
         Work to Progressively
-Improve
-Having mastered the basics
+      </p>
+      <div class="text--primary">
+Improve having mastered the basics
 our aim is to continue
 working with you to bring
 you to an intermediate and
@@ -79,6 +118,12 @@ level where you may even
 swim competitively as part
 of a swim club if you
 choose.
+      </div>
+    </v-card-text>
+
+
+      
+
 
       </v-card>
 
@@ -87,33 +132,45 @@ choose.
 
 
 
+    </v-container>
+
+
+    <div style="background-color: #80deff;" class="text-center py-12 my-12">
+     <p>Join Our Learn To Swim Programme </p>
     </div>
 
 
-    <div>
-      Join Our Learn To Swim Programme (Takes them to form to sign-up)
-    </div>
 
-
-
-    <div>
+    <div class="text-center py-md-12 my-12">
+          <p class="display-1 text--primary">
       Contact Us
-
-At Island Blue Pools we have developed learn to swim curriculums for all age groups, ranging
+      </p>
+      <p>
+        At Island Blue Pools we have developed learn to swim curriculums for all age groups, ranging
 from toddlers to senior citizens. Swimmers have been amazed by the results they achieve while
 participating in our programme. We are happy to have helped swimmers overcome their fear of
-drowning and their own doubts to be able to swim with confidence. Contact us to get started
+drowning and their own doubts to be able to swim with confidence. 
+      </p>
 
-quickly and with confidence as well.
+
+
+<p>Contact us to get started quickly and with confidence.</p>
+
+
+
     </div>
 
-    <div>
-      Island Blue Pools
-
-Our vision is to provide a platform for every Jamaican to learn how to swim. Approximately 15%
-of Jamaicans can swim, which is a relatively small number for a country surrounded by water. The
+    <div style="background-color: #80deff;" class="text-center py-12 my-12">
+       <p class="display-1 text--primary mb-0">
+       Island Blue Pools
+      </p>
+     <p class="px-16">Our vision is to provide a platform for every Jamaican to learn how to swim. </p>
+<p class="px-16 text-left">
+  Approximately 15% of Jamaicans can swim, which is a relatively small number for a country surrounded by water. The
 team at Island Blue Pools is focused on changing this statistic, and believe the quality of our
 instruction, affordability and passion for swimming will drive us forward to achieve our vision.
+</p>
+
     </div>
 
     <div>
@@ -134,3 +191,32 @@ export default {
 
 }
 </script>
+
+<style scoped>
+
+.video-section {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.site-content {
+  background-color: #fff;
+  text-align: center;
+  z-index: 1;
+  opacity: 0.8;
+}
+
+.video-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+}
+
+</style>
